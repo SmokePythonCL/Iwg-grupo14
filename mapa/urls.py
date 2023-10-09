eaa7c6bb7164c7fbd41c42a618b4f9148e435238
Kran <kran.olivares.rojas@gmail.com>
2023-10-09 11:28:54 -0300

    Formato tipo Django Ajuste de HTML y archivos a carpeta static.

--- mapa/urls.py
+++ mapa/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.map)
+    path('', views.index),
+    path('status/', views.status),
+    path('route/', views.route)
     ]
--- mapa/views.py
+++ mapa/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def map(request):
-    return render(request, 'mapa/base.html')
+    return render(request, 'mapa/map.html')
+
+def index(request):
+    return render(request, 'mapa/index.html')
+
+def status(request):
+    return render(request, 'mapa/status.html')
+
+def route(request):
+    return render(request, 'mapa/route.html')
