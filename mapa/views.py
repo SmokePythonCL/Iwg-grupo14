daa9a286c8f3a1d8965229729a3205c5dcd2f1cf
Kran Olivares <kran.olivares.rojas@gmail.com>
2023-10-10 23:32:16 -0300

    Carga de puntos en el mapa y principio interactivo de los mismo.

--- mapa/views.py
+++ mapa/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import CoordsModel
 
 # Create your views here.
 
@@ -8,8 +9,22 @@ def map(request):
 def index(request):
     return render(request, 'mapa/index.html')
 
-def status(request):
-    return render(request, 'mapa/status.html')
-
 def route(request):
     return render(request, 'mapa/route.html')
+
+def status(request):
+    points = CoordsModel.objects.all()
+    ascensor = []
+    rampa = []
+
+    for item in points:
+        coord_type = item.point_type
+        coord = item.coords
+
+        if coord_type == "0":
+            ascensor.append(coord)
+        
+        elif coord_type == "1":
+            rampa.append(coord)
+
+    return render(request, 'mapa/status.html', {"ascensor": ascensor, "rampa": rampa})
