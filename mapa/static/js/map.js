09dc6398062df932867e28e3452a3caff87462af
Kran <kran.olivares.rojas@gmail.com>
2023-10-21 22:09:41 -0300

    Mapa con capas y función onClick a los puntos.

--- mapa/static/js/map.js
+++ mapa/static/js/map.js
@@ -1,17 +1,31 @@
+// Variables
+var bounds = [[0,0], [1000,1000]];
+var basemap = L.imageOverlay('/static/images/CAMPUS-SJ.png', bounds,
+            {attribution: "USM SJ"});
+var secondfloor = L.imageOverlay('/static/images/testmap2.jpg', bounds,
+            {attribution: "USM SJ"});
+var thirdfloor = L.imageOverlay('/static/images/testmap.jpg', bounds,
+            {attribution: "USM SJ"});
+//var fourthfloor = L.imageOverlay('/static/images/', bounds, {attribution: "USM SJ"});
+//var fifthfloor = L.imageOverlay('/static/images/', bounds, {attribution: "USM SJ"});
+
+var basemaps = {
+    "Primer Piso": basemap,
+    "Segundo Piso": secondfloor,
+    "Tercer Piso": thirdfloor,
+    //"Cuarto Piso": fourthfloor,
+    //"Quinto Piso": fifthfloor,
+}
+
 var map = L.map('map', {
     crs: L.CRS.Simple,
     minZoom: 0,
-    maxZoom: 4
+    maxZoom: 4,
+    layers: [basemap],
+    attribution: "USM SJ"
 });
 
-var bounds = [[0,0], [1000,1000]];
-var image = L.imageOverlay('/static/images/CAMPUS-SJ.png', bounds).addTo(map);
-map.setView( [500, 500], 0);
-map.setMaxBounds(bounds);
-map.on('drag', function () {
-    map.panInsideBounds(bounds, { animate: false });
-});
-
+var layerControl = L.control.layers(basemaps).addTo(map);
 
 var yx = L.latLng;
 var xy = function(x, y) {
@@ -36,21 +50,29 @@ var greenLift = new baseIcon({iconUrl: '/static/icons/elevator-solid.svg',
                             className: "yellow-elevator"}),
                             
     redLift = new baseIcon({iconUrl: '/static/icons/elevator-solid.svg', 
-                            className: "red-elevator"});
+                            className: "red-elevator"}),
 
-var greenSlope = new baseIcon({iconUrl: '/static/icons/ramp.svg',
+    greenSlope = new baseIcon({iconUrl: '/static/icons/ramp.svg',
                             className: "green-slope"}),
+
     yellowSlope = new baseIcon({iconUrl: '/static/icons/ramp.svg',
                             className: "yellow-slope"}),
-    redSlope = new baseIcon({iconUrl: '/static/icons/ramp.svg',
-                            className: "red-slope"});
 
-var greenOther = new baseIcon({iconUrl: '/static/icons/ramp.svg', 
+    redSlope = new baseIcon({iconUrl: '/static/icons/ramp.svg',
+                            className: "red-slope"}),
+
+    greenOther = new baseIcon({iconUrl: '/static/icons/ramp.svg', 
                                 className: "green-slope"}),
+
     yellowOther = new baseIcon({iconUrl: '/static/icons/ramp.svg', 
                                 className: "yellow-slope"}),
+
     redOther = new baseIcon({iconUrl: '/static/icons/ramp.svg', 
                                 className: "red-slope"});
+
+function onClick(e) {
+    alert(this.getLatLng());
+}
 
 var coords = JSON.parse(document.querySelector("#map").getAttribute('data-json'));
 var coordenadas = []
@@ -60,26 +82,32 @@ var estado = null
 for (var tipo in coords) {
     coordenadas = coords[tipo][0];
     punto = xy(coordenadas[0], coordenadas[1]);
-    estado = coordenadas[2].concat(tipo); // Por alguna razón hacer que varíe no funciona.
-    console.log(estado);
+    estado = coordenadas[2].concat(tipo);
 
     if (estado === "greenSlope") {
-        L.marker(punto, {icon: greenSlope}).addTo(map).bindPopup(tipo);
+        L.marker(punto, {icon: greenSlope}).addTo(map).bindPopup(tipo).on('click', onClick);
     } else if (estado === "yellowSlope") {
-        L.marker(punto, {icon: yellowSlope}).addTo(map).bindPopup(tipo);
+        L.marker(punto, {icon: yellowSlope}).addTo(map).bindPopup(tipo).on('click', onClick);
     } else if (estado === "redSlope") {
-        L.marker(punto, {icon: redSlope}).addTo(map).bindPopup(tipo);
+        L.marker(punto, {icon: redSlope}).addTo(map).bindPopup(tipo).on('click', onClick);
     } else if (estado === "greenLift") {
-        L.marker(punto, {icon: greenLift}).addTo(map).bindPopup(tipo);
+        L.marker(punto, {icon: greenLift}).addTo(map).bindPopup(tipo).on('click', onClick);
     } else if (estado === "yellowLift") {
-        L.marker(punto, {icon: yellowLift}).addTo(map).bindPopup(tipo);
+        L.marker(punto, {icon: yellowLift}).addTo(map).bindPopup(tipo).on('click', onClick);
     } else if (estado === "redLift") {
-        L.marker(punto, {icon: redLift}).addTo(map).bindPopup(tipo);
+        L.marker(punto, {icon: redLift}).addTo(map).bindPopup(tipo).on('click', onClick);
     } else if (estado === "greenOther") {
-        L.marker(punto, {icon: greenOther}).addTo(map).bindPopup(tipo);
+        L.marker(punto, {icon: greenOther}).addTo(map).bindPopup(tipo).on('click', onClick);
     } else if (estado === "yellowOther") {
-        L.marker(punto, {icon: yellowOther}).addTo(map).bindPopup(tipo);
+        L.marker(punto, {icon: yellowOther}).addTo(map).bindPopup(tipo).on('click', onClick);
     } else if (estado === "redOther") {
-        L.marker(punto, {icon: redOther}).addTo(map).bindPopup(tipo);
+        L.marker(punto, {icon: redOther}).addTo(map).bindPopup(tipo).on('click', onClick);
     }
 }
+
+// Map set
+map.setView( [500, 500], 0);
+map.setMaxBounds(bounds);
+map.on('drag', function () {
+    map.panInsideBounds(bounds, { animate: false });
+});
